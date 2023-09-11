10e255692b5babf906815d626a718fb02a176f35
JaxxyIV <82413682+JaxxyIV@users.noreply.github.com>
2023-09-11 19:01:52 -0400

    Remove dist from repo

--- dist/structs/classes/ShopItem.d.ts
+++
@@ -1,8 +0,0 @@
-import { ShopItemData } from "../../types/apiStructs";
-export default class ShopItem {
-    #private;
-    constructor(json: ShopItemData);
-    get item(): string;
-    get price(): number;
-    get [Symbol.toStringTag](): string;
-}
--- dist/structs/util/Request.d.ts
+++
@@ -1,8 +0,0 @@
-export default class Request {
-    #private;
-    constructor(path: string);
-    get(as: ResponseType): Promise<any>;
-    post(body: any, as: ResponseType, headers?: HeadersInit): Promise<any>;
-}
-type ResponseType = "json" | "text" | "buffer";
-export {};
