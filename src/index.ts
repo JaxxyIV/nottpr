9be8c0c9654b3f16dc1be9eb10b1f58ba08870bc
JaxxyIV <82413682+JaxxyIV@users.noreply.github.com>
2023-09-13 17:16:40 -0400

    export Hash.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { default } from "./structs/util/ALTTPR";
 export { default as SeedBuilder } from "./structs/classes/SeedBuilder";
 export { default as MysteryWeightset } from "./structs/classes/MysteryWeightset";
+export { default as RegionBuilder } from "./structs/classes/RegionBuilder";
+export { Hash } from "./types/enums";
