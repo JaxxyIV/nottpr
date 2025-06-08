c07beb14fd86e39a189b86accdc408362df74f76
JaxxyIV <82413682+JaxxyIV@users.noreply.github.com>
2025-06-08 15:59:54 -0400

    Remove redundancy

--- src/classes/builder/CustomizerCustomBuilder.ts
+++
@@ -1,8 +0,0 @@
-import { CustomizerCustomOptions } from "../../types/apiStructs";
-import JSONTranslatable from "../interfaces/JSONTranslatable";
-
-export default class CustomizerCustomBuilder implements JSONTranslatable {
-    toJSON(): unknown {
-        throw new Error("Method not implemented.");
-    }
-}
--- src/classes/builder/CustomizerDropsBuilder.ts
+++
@@ -1,9 +0,0 @@
-import { PrizePackGroups } from "../../types/apiStructs";
-import JSONTranslatable from "../interfaces/JSONTranslatable";
-
-export default class CustomizerDropsBuilder implements JSONTranslatable {
-    toJSON(): unknown {
-        throw new Error("Method not implemented.");
-    }
-
-}
